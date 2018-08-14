cdd29376ab01a9ad79c673bf8c0110c98d76058e
Tri Le <tri.le@oddle.me>
2018-08-14 23:46:08 +0800

    Initial commit

---
+++ packages/core/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  height: 50px;
+  background-color: yellow;
+`
+
+export default Button
---
+++ packages/fesc/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from 'core/Button'
+
+class Index extends React.PureComponent {
+  render () {
+    return (
+      <Button>
+        Hello World
+      </Button>
+    )
+  }
+}
+
+export default Index
