19643d9f1c2dfa7e7a1e14e574dd33c65133f95d
Tri Le <tri.le@oddle.me>
2018-09-11 14:06:55 +0800

    Update button

---
+++ Button/Button.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import Div from 'Div'
+
+const Button = styled.button`
+  height: 60px;
+  font-weight: bold;
+  color: white;
+  background-color: blue;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    background-color: red;
+  }
+  &:focus {
+    background-color: green;
+  }
+`
+
+export default (props) => (
+  <Div>
+    <Button {...props} />
+  </Div>
+)
